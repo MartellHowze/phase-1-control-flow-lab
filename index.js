db0e11b5aa97b2286d10ac0cae61764d60d43688
Martell Howze <martellhowze042988@gmail.com>
2021-08-14 18:44:50 -0500

    commit tree 1af78b6d086468918f5f5a643e4e39c2ad459aee

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
+if (number <= 400) {
+  return 'This one is on me!'
+  }
+else if (number <= 2499 ){
+  return 'I will gladly take your thirty bucks.'
+
+  }
+else {
+  return 'No can do.'
+}
+
+}
+const city = 'NYC';
+function ternaryCheckCity(place){
+  return place ==='NYC' ? "Ok, sounds good." : "No go.";
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
+  switch(amount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
   // Write your code here!
 }
